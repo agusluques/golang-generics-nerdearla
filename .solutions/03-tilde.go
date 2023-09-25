c7bd52cd4d3074d7706bc21ea272e5269c1e7f29
Nicolas Del Piano <ndel314@gmail.com>
2023-09-25 20:33:11 -0300

    Minor comments

--- .solutions/03-tilde.go
+++ .solutions/03-tilde.go
@@ -2,6 +2,7 @@ package main
 
 import "fmt"
 
+// ✅ Ejercicio 1:
 func GetFloatsIn2Decimals[F ~float32 | ~float64](f F) string {
 	return fmt.Sprintf("%.2f", f)
 }
--- .solutions/05-inference.go
+++ .solutions/05-inference.go
@@ -2,6 +2,7 @@ package main
 
 import "golang.org/x/exp/constraints"
 
+// ✅ Ejercicio 1:
 func ScaleAllElements[I constraints.Integer, R ~[]I](list R, factor I) R {
 	for i := range list {
 		list[i] *= factor
