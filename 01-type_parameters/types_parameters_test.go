286384bd495cae953226d5746e3cd58bad5436d8
Nicolas Del Piano <ndel314@gmail.com>
2023-09-26 23:35:00 -0300

    Fix tests in type parameters

--- 01-type_parameters/types_parameters_test.go
+++ 01-type_parameters/types_parameters_test.go
@@ -1,14 +1,69 @@
 package typeparameters
 
 import (
+	"reflect"
 	"testing"
-
-	"github.com/stretchr/testify/assert"
 )
 
-type testCase struct {
+func TestMapFunctions(t *testing.T) {
+	testCases := []struct {
+		name     string
+		input    interface{}
+		expected []interface{}
+		f        interface{}
+	}{
+		{
+			name:     "MapStrings",
+			input:    []string{"apple", "banana", "cherry"},
+			expected: []interface{}{5, 6, 6},
+			f: func(input interface{}) interface{} {
+				return len(input.(string))
+			},
+		},
+		{
+			name:     "MapInts",
+			input:    []int{1, 2, 3, 4},
+			expected: []interface{}{2, 4, 6, 8},
+			f: func(input interface{}) interface{} {
+				return input.(int) * 2
+			},
+		},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.name, func(t *testing.T) {
+			result := callMapFunction(testCase.input, testCase.f)
+			if !reflect.DeepEqual(result, testCase.expected) {
+				t.Errorf("Expected %v, but got %v", testCase.expected, result)
+			}
+		})
+	}
 }
 
-func TestSuma(t *testing.T) {
-	assert.Equalf(t, MapStrings([]string{"1"}, func(s string) interface{} {}), Map(1, 2), "Add(1, 2) should be 3")
+func callMapFunction(input interface{}, f interface{}) []interface{} {
+	inputValue := reflect.ValueOf(input)
+	funcValue := reflect.ValueOf(f)
+
+	if inputValue.Kind() != reflect.Slice {
+		panic("Input must be a slice")
+	}
+
+	if funcValue.Kind() != reflect.Func {
+		panic("Function must be a function")
+	}
+
+	result := reflect.MakeSlice(reflect.SliceOf(reflect.TypeOf(f).Out(0)), inputValue.Len(), inputValue.Len())
+
+	for i := 0; i < inputValue.Len(); i++ {
+		params := []reflect.Value{inputValue.Index(i)}
+		output := funcValue.Call(params)[0]
+		result.Index(i).Set(output)
+	}
+
+	resultInterface := make([]interface{}, result.Len())
+	for i := 0; i < result.Len(); i++ {
+		resultInterface[i] = result.Index(i).Interface()
+	}
+
+	return resultInterface
 }
