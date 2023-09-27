b23243e92beea1ee9d1d0cca4f8003d93ff24d46
Nicolas Del Piano <ndel314@gmail.com>
2023-09-27 01:23:13 -0300

    Moar on tilde

--- tilde/tilde.go
+++ tilde/tilde.go
@@ -1,1 +1,10 @@
 package tilde
+
+type ID int64
+
+func Process[T int64](value T) {
+	// ...
+}
+
+var id ID = 10
+Process(id) // error ❌: ID does not satisfy int64 (possibly missing ~ for int64 in int64)
